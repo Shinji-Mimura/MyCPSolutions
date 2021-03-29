920b80ea98af6a06d9d2584971190bc2fce23ffd
Lucas Araújo <59016360+Shinji-Mimura@users.noreply.github.com>
2021-03-29 12:35:36 -0300

    Update WeightForWeight.py

--- Codewars/WeightForWeight.py
+++ Codewars/WeightForWeight.py
@@ -10,13 +10,10 @@ def order_weight(string):
     lista_numeros = string.split(" ")
     lista_numeros = sorted(lista_numeros)
     lista_soma_numeros = []
-    somador = 0
     
     for n in lista_numeros:
-        for i in range(len(n)):
-            somador += int(n[i])
+        somador = sum_numbers(n)
         lista_soma_numeros.append(somador)
-        somador = 0
         
     lista_soma_numeros = list(set(lista_soma_numeros))
     lista_soma_numeros.sort()
